7292826e6e2b3cbb2a4f7ed78508f8a7bff53e5b
lonelynight-dark <52921466+lonelynight-dark@users.noreply.github.com>
2021-11-11 01:41:01 +0700

    Function 5: Delete COLUMNS with percentage of missing values greater than threshold

--- list-missing.py
+++ list-missing.py
@@ -1,40 +1,38 @@
 import numpy as np
 import pandas as pd
-import math
-import sys, getopt
+import sys
 
-from pandas.core.dtypes.missing import isnull
-
-def isMissingValue(col):
+def isMissingValueColumn(col):
+    """
+    check if a column having missing value
+    @param col column 
+    @return true if column having missing value
+    """
     return any(isNaN(item) for item in col)
 
 def isNaN(item):
+    """
+    check if an item is NaN (missing value)
+    @param item an item
+    @return true if that item is NaN
+    """
     return item != item # python > 2.5 required
 
 def main(argv):
+    # check argv from command line
     if (len(argv) != 1):
         print('usage:python list-missing.py <inputfile>')
         sys.exit(2)
+
+    # read file
     inputfile = argv[0]
-    """
-    try:
-        opts, args = getopt.getopt(argv,"","")
-    except getopt.GetoptError:
-        print('list-missing.py <inputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('list-missing.py <inputfile>') 
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-    """
     df = pd.read_csv(inputfile)
 
+    # list all columns having missing value
     print("Columns having missing values:", end="")
     for column in df:
-        if isMissingValue(df[column].to_list()):
+        if isMissingValueColumn(df[column]):
             print(column, end=",")
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1:]) # ignore --.py
